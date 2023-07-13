3ea7f1b4a1d75aa68ccd2a97d102e2dc38adbe7d
PShubh619 <136714333+PShubh619@users.noreply.github.com>
2023-07-13 15:06:19 +0530

    databinding Complited

--- app/src/main/java/com/example/location_source/Model.kt
+++ app/src/main/java/com/example/location_source/Model.kt
@@ -1,4 +1,12 @@
 package com.example.location_source
 
-class Model {
+class Model (val Location:String,
+             val Address:String?,
+             val Distance:Double,
+             var Longitude: String,
+             var Latitude: String,
+             var IsPrimary:Boolean) {
+
+
+
 }
